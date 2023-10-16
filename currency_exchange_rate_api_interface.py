96ff22da7adcbed180eed76bb08be5fac806b173
gmtev <gmtev1007@gmail.com>
2023-10-16 08:35:10 +0300

    Update currency_exchange_rate_api_interface.py

--- currency_exchange_rate_api_interface.py
+++ currency_exchange_rate_api_interface.py
@@ -1,115 +1,26 @@
 import requests
 
 
-def get_price_eur_usd():
-    url = "https://www.freeforexapi.com/api/live?pairs=EURUSD"
+def get_price(cur_pair):
+    url = f"https://www.freeforexapi.com/api/live?pairs={cur_pair}"
     response = requests.get(url)
     if response.status_code == 200:
         date = response.json()
-        rate = date["rates"]["EURUSD"]["rate"]
-        return f'Current rate for EUR to USD is: {rate}'
+        rate = date["rates"][f"{cur_pair}"]["rate"]
+        return f'Current rate for {cur_pair[:3]} to {cur_pair[3:]} is: {rate}'
     else:
         return 'Error fetching data!'
 
 
-def get_price_eur_gbp():
-    url = "https://www.freeforexapi.com/api/live?pairs=EURGBP"
-    response = requests.get(url)
-    if response.status_code == 200:
-        date = response.json()
-        rate = date["rates"]["EURGBP"]["rate"]
-        return f'Current rate for EUR to GBP is: {rate}'
-    else:
-        return 'Error fetching data!'
-
-
-def get_price_gbp_usd():
-    url = "https://www.freeforexapi.com/api/live?pairs=GBPUSD"
-    response = requests.get(url)
-    if response.status_code == 200:
-        date = response.json()
-        rate = date["rates"]["GBPUSD"]["rate"]
-        return f'Current rate for GBP to USD is: {rate}'
-    else:
-        return 'Error fetching data!'
-
-
-def get_price_usd_jpy():
-    url = "https://www.freeforexapi.com/api/live?pairs=USDJPY"
-    response = requests.get(url)
-    if response.status_code == 200:
-        date = response.json()
-        rate = date["rates"]["USDJPY"]["rate"]
-        return f'Current rate for USD to JPY is: {rate}'
-    else:
-        return 'Error fetching data!'
-
-
-def get_price_aud_usd():
-    url = "https://www.freeforexapi.com/api/live?pairs=AUDUSD"
-    response = requests.get(url)
-    if response.status_code == 200:
-        date = response.json()
-        rate = date["rates"]["AUDUSD"]["rate"]
-        return f'Current rate for AUD to USD is: {rate}'
-    else:
-        return 'Error fetching data!'
-
-
-def get_price_usd_chf():
-    url = "https://www.freeforexapi.com/api/live?pairs=USDCHF"
-    response = requests.get(url)
-    if response.status_code == 200:
-        date = response.json()
-        rate = date["rates"]["USDCHF"]["rate"]
-        return f'Current rate for USD to CHF is: {rate}'
-    else:
-        return 'Error fetching data!'
-
-
-def get_price_nzd_usd():
-    url = "https://www.freeforexapi.com/api/live?pairs=NZDUSD"
-    response = requests.get(url)
-    if response.status_code == 200:
-        date = response.json()
-        rate = date["rates"]["NZDUSD"]["rate"]
-        return f'Current rate for NZD to USD is: {rate}'
-    else:
-        return 'Error fetching data!'
-
-
-def get_price_usd_cad():
-    url = "https://www.freeforexapi.com/api/live?pairs=USDCAD"
-    response = requests.get(url)
-    if response.status_code == 200:
-        date = response.json()
-        rate = date["rates"]["USDCAD"]["rate"]
-        return f'Current rate for USD to CAD is: {rate}'
-    else:
-        return 'Error fetching data!'
-
-
-def get_price_usd_zar():
-    url = "https://www.freeforexapi.com/api/live?pairs=USDZAR"
-    response = requests.get(url)
-    if response.status_code == 200:
-        date = response.json()
-        rate = date["rates"]["USDZAR"]["rate"]
-        return f'Current rate for USD to ZAR is: {rate}'
-    else:
-        return "Error fetching data!"
-
-
-current_rate_eur_usd = get_price_eur_usd()
-current_rate_eur_gbp = get_price_eur_gbp()
-current_rate_gbp_usd = get_price_gbp_usd()
-current_rate_usd_jpy = get_price_usd_jpy()
-current_rate_aud_usd = get_price_aud_usd()
-current_rate_usd_chf = get_price_usd_chf()
-current_rate_nzd_usd = get_price_nzd_usd()
-current_rate_usd_cad = get_price_usd_cad()
-current_rate_usd_zar = get_price_usd_zar()
-
+eur_usd = 'EURUSD'
+eur_gbp = "EURGBP"
+gbp_usd = "GBPUSD"
+usd_jpy = "USDJPY"
+aud_usd = "AUDUSD"
+usd_chf = "USDCHF"
+nzd_usd = "NZDUSD"
+usd_cad = "USDCAD"
+usd_zar = "USDZAR"
 print(f'''Welcome to the currency exchange rates API interface!
 The available options are:
         "EURUSD" - 1
@@ -129,22 +40,23 @@ while True:
         print("Have a nice day!")
         break
     elif user_input == "1":
-        print(get_price_eur_usd())
+        print(get_price(eur_usd))
     elif user_input == "2":
-        print(get_price_eur_gbp())
+        print(get_price(eur_gbp))
     elif user_input == "3":
-        print(get_price_gbp_usd())
+        print(get_price(gbp_usd))
     elif user_input == "4":
-        print(get_price_usd_jpy())
+        print(get_price(usd_jpy))
     elif user_input == "5":
-        print(get_price_aud_usd())
+        print(get_price(aud_usd))
     elif user_input == "6":
-        print(get_price_usd_chf())
+        print(get_price(usd_chf))
     elif user_input == "7":
-        print(get_price_nzd_usd())
+        print(get_price(nzd_usd))
     elif user_input == "8":
-        print(get_price_usd_cad())
+        print(get_price(usd_cad))
     elif user_input == "9":
-        print(get_price_usd_zar())
+        print(get_price(usd_zar))
     else:
         print("Incorrect input, try again!")
+
